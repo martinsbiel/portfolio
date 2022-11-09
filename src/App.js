4f2f9b81fcbbb0a3bb7ed0a14d12c3a2c3b9496b
Gabriel Martins <martinsbiel@hotmail.com>
2022-11-09 17:44:07 -0300

    Update age and project details

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             <h2 className="text-center header-text">Gabriel Martins</h2>
             <h5 className="text-center mb-4 header-text">Desenvolvedor de Software Jr</h5>
             <h5 className="border-bottom pb-2 header-text">Sobre mim</h5>
-            <p className="text-white about">Tenho 24 anos, nasci em Joinville, mas já morei no interior de SP. A única coisa que nunca mudou é meu interesse desde sempre por tecnologia. Encontrei na tecnologia a oportunidade de estar em constante desafio.</p>
+            <p className="text-white about">Tenho 25 anos, nasci em Joinville, mas já morei no interior de SP. A única coisa que nunca mudou é meu interesse desde sempre por tecnologia. Encontrei na tecnologia a oportunidade de estar em constante desafio.</p>
             <p className="text-white about">Gosto de estar em constante movimento, estudando sobre boas práticas de programação e desenvolvendo experimentos para descobrir e experimentar novas resoluções. Me considero um profissional motivado por resolver problemas, agilizar processos e criar soluções de qualidade, utilizando as técnicas e tecnologias adequadas.</p>
             <p className="text-white">Vamos nessa?</p>
             <a href="https://github.com/martinsbiel" target="_blank" className="link-about mb-1"><i className="fab fa-github-square"></i> GitHub</a>
@@ -22,7 +22,7 @@ function App() {
             <h5 className="border-bottom pb-2 header-text mt-5">Meus objetivos</h5>
             <p className="text-white">Meu objetivo é entrar para o mercado de trabalho como um desenvolvedor de software junior, atuando remotamente.</p>
             <h5 className="border-bottom pb-2 header-text mt-5">Minhas habilidades</h5>
-            <p className="text-white">Possuo fortes conhecimentos em: PHP, Laravel, JavaScript ES6+, jQuery e MySQL</p>
+            <p className="text-white">Possuo fortes conhecimentos em: PHP, Laravel, JavaScript ES6+, jQuery, Bootstrap e MySQL.</p>
             <p className="text-white">Estou familiarizado com: VueJS e NodeJS.</p>
             <h5 className="border-bottom pb-2 header-text mt-5">Meus projetos</h5>
             <p className="text-white">Com o passar do tempo e o avançar dos meus estudos decidi colocar tudo que aprendi em prática, abaixo estão as informações de alguns projetos que fiz:</p>
@@ -35,7 +35,7 @@ function App() {
             
             <p className="text-white"><a target="_blank" className="link-light" href="https://uploaderweb.com">UploaderWeb</a>: Meu projeto mais antigo, criando no início de 2014. O UploaderWeb é uma comunidade online criada para compartilhamento de arquivos em geral <a className="link-light" href="#" data-bs-toggle="modal" data-bs-target="#upwebModal">&rarr; Leia mais...</a></p>
         </div>
-        <Modal id="notesModal" name="Notes" year="2021" link="https://github.com/martinsbiel/notes" linkText="Link para o repositório do projeto, contribua." content="Sistema de controle de notas/lembretes. Crie, gerencie e exporte suas notas em um sistema muito completo. Utilizei Laravel para a API REST com autenticação JWT e por sessão no back-end e VueJS para deixar o front-end mais dinâmico em conjunto com Bootstrap. Contém recurso de exportar notas para Excel e PDF, registro de logs em todos os endpoints da API de notas e sistema de login completo. Código fonte disponível no repositório do projeto, toda contribuição é bem-vinda.">
+        <Modal id="notesModal" name="Notes" year="2021" link="https://github.com/martinsbiel/notes" linkText="Link para o repositório do projeto, contribua." content="Sistema de controle de notas/lembretes. Crie, gerencie e exporte suas notas em um sistema muito completo. Utilizei Laravel para a API REST com autenticação JWT e por sessão no back-end e VueJS para deixar o front-end mais dinâmico em conjunto com Bootstrap. Contém recurso de exportar notas para Excel e PDF, registro de logs em todos os endpoints da API de notas e sistema de login completo. Também implementei Design Pattern Repository e testei todos os endpoints da API utilizando PHPUnit. Código fonte disponível no repositório do projeto, toda contribuição é bem-vinda.">
             <Badge type="bg-primary">Laravel</Badge>
             <Badge type="bg-success">VueJS</Badge>
             <Badge type="bg-secondary">Bootstrap</Badge>
